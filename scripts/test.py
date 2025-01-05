2a23d18a10f009aa2111909f41ee3240c9152f15
Nathan Whitehead <nwhitehe@gmail.com>
2025-01-05 07:30:52 -0700

    Working extraction from TIMIT.

--- scripts/test.py
+++ scripts/test.py
@@ -5,6 +5,7 @@ import pyarrow.parquet as pq
 import os
 import argparse
 import random
+import torch
 import torchaudio
 import pyloudnorm as pyln
 
@@ -12,22 +13,23 @@ table = pq.read_table(os.path.join('data', 'TIMIT', 'train-00000-of-00002.parque
 
 rate = 16000
 meter = pyln.Meter(rate)
-level = -20.0
 
-def extract(n):
+def extract(n, level=-20.0):
     data = table['audio'][n]['array']
     # Normalize each sample before concat
     loudness = meter.integrated_loudness(data)
     return pyln.normalize.loudness(data, loudness, level)
 
-def extracts(duration, seed=1234, gap=0.5):
+def extracts(duration, seed=1234, gap=0.5, level=-20.0):
     silence = np.zeros((round(gap * rate), ))
     random.seed(seed)
     index = list(range(table.shape[0]))
     random.shuffle(index)
     res = np.zeros((0,))
     while res.shape[0] < duration * rate and len(index) > 0:
-        res = np.concatenate([res, extract(index.pop()), silence])
+        i = index.pop()
+        print(f'Extracting sentence {i}')
+        res = np.concatenate([res, extract(i, level=level), silence])
     return res
 
 def main():
@@ -35,9 +37,15 @@ def main():
     parser.add_argument('--duration', type=float, default=5.0, metavar='SEC', help='how many seconds of audio to request (may be exceeded)')
     parser.add_argument('--seed', type=int, default=1234, help='random seed to use')
     parser.add_argument('--gap', type=float, default=0.5, metavar='SEC', help='how long between sentences')
+    parser.add_argument('--level', type=float, default=-20.0, metavar='LUFS', help='perceptual volume level')
+    parser.add_argument('--output', default='out.mp3', help='output file')
     args = parser.parse_args()
-    waveform = extracts(duration=args.duration, seed=args.seed, gap=args.gap)
     print(args)
+    waveform = extracts(duration=args.duration, seed=args.seed, gap=args.gap, level=args.level)
+    total_duration = waveform.shape[0] / rate
+    print(f'Total duration: {total_duration} s')
+    print(f'Writing output: {args.output}')
+    torchaudio.save(args.output, torch.Tensor(waveform.reshape([1, -1])), sample_rate=rate, backend='ffmpeg')
 
 if __name__ == '__main__':
     main()
