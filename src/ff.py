02d64983e4f9140f4c2301f1d10c419c3df18be0
Nathan Whitehead <nwhitehe@gmail.com>
2025-01-08 12:42:07 -0700

    Add test step to train script.

--- src/ff.py
+++ src/ff.py
@@ -24,6 +24,7 @@ class NeuralNet(nn.Module):
             nn.Linear(input_size, hidden_size),
             nn.ReLU(),
             nn.Linear(hidden_size, num_classes),
+            nn.Softmax(),
         ]
         self.net = nn.Sequential(*layers)
 
@@ -216,7 +217,7 @@ input_size = feature_dims * lookahead_frames
 hidden_size = 500
 num_classes = len(viseme_labels)
 num_epochs = 5
-batch_size = 10
+batch_size = 20
 learning_rate = 0.001
 batch_time = 200
 
@@ -236,11 +237,18 @@ transform = nn.Sequential(
 dataset = LipsyncDataset('./data/lipsync.parquet', transform=transform)
 
 rng = torch.Generator().manual_seed(1)
-train_dataset, test_dataset = torch.utils.data.random_split(dataset, [0.05, 0.95], generator=rng)
+train_dataset, test_dataset, _ = torch.utils.data.random_split(dataset, [0.30, 0.10, 0.6], generator=rng)
 
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
-                                           batch_size=batch_size, 
-                                           shuffle=True)
+train_loader = torch.utils.data.DataLoader(
+    dataset=train_dataset, 
+    batch_size=batch_size, 
+    shuffle=True,
+)
+test_loader = torch.utils.data.DataLoader(
+    dataset=test_dataset,
+    batch_size=batch_size,
+    shuffle=False,
+)
 
 total_step = len(train_dataset)
 for epoch in tqdm(range(num_epochs), total=num_epochs, desc='Epoch', colour='#FF80D0'):
@@ -250,31 +258,33 @@ for epoch in tqdm(range(num_epochs), total=num_epochs, desc='Epoch', colour='#FF
         # audio is B C T -> float
         audio = sample['audio'].to(device)
         # visemes is B T -> float representing viseme
-        visemes = sample['visemes'].to(device)
+        visemes = sample['visemes'].to(torch.long).to(device)
 
         # Forward passes
         for offset in range(batch_time - lookahead_frames + 1):
             inputs = audio[:, :, offset:offset + lookahead_frames].reshape(-1, input_size).to(torch.float)
             labels = visemes[:, offset]
-#            print(inputs.shape, labels.shape)
             outputs = model(inputs)
-            print(outputs.shape)
-#         loss = criterion(outputs, labels)
+            loss = criterion(outputs, labels)
         
-#         # Backward and optimize
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-        
-#         if (i+1) % 100 == 0:
-#             print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-#                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-# def demo():
-#     dataset = LipsyncDataset('./data/lipsync.parquet')
-#     print(len(dataset))
-#     for i, sample in enumerate(dataset):
-#         print(i, sample['audio'].shape, sample['visemes'].shape)
+    with torch.no_grad():
+        correct = 0
+        total = 0
 
-# if __name__ == '__main__':
-#     demo()
+    # Validation step
+    for i, sample in tqdm(enumerate(test_loader), total=len(test_loader), desc='Sample', leave=False, colour='#FFD0FF'):
+        audio = sample['audio'].to(device)
+        visemes = sample['visemes'].to(torch.long).to(device)
+        for offset in range(batch_time - lookahead_frames + 1):
+            inputs = audio[:, :, offset:offset + lookahead_frames].reshape(-1, input_size).to(torch.float)
+            labels = visemes[:, offset]
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print(f'Accuracy on {len(test_loader)} samples: {100 * correct / total} %')
