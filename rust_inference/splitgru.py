0d01985ab7bf2d65984c795e864c0d825d7a1e50
Nathan Whitehead <nwhitehe@gmail.com>
2025-01-13 10:58:42 -0700

    Working on remapping weights.

---
+++ rust_inference/splitgru.py
@@ -0,0 +1,21 @@
+import torch
+from collections import OrderedDict
+
+s = torch.load('./model-2-80-dropout.pt', map_location=torch.device('cpu'))
+out = OrderedDict(s)
+for k in s:
+    if '_ih_' in k or '_hh_' in k:
+        r = s[k].shape[0]
+        if r % 3 != 0:
+            raise Exception('Dimension size of layer not a multiple of 3')
+        h = r // 3
+        for i in range(3):
+            if 'bias' in k:
+                x = s[k][i * h:i * h + h]
+            else:
+                x = s[k][i * h:i * h + h, :]
+            out[f'{k}.{['r', 'z', 'n'][i]}'] = x
+    else:
+        out[k] = s[k]
+
+torch.save(out, './model-2-80-dropout.ptx')
--- rust_inference/src/main.rs
+++ rust_inference/src/main.rs
@@ -1,23 +1,41 @@
-use crate::model::ModelConfig;
 use crate::model::Model;
+use crate::model::ModelConfig;
 use burn::backend::NdArray;
+use burn::module::Module;
+use burn::record::NamedMpkFileRecorder;
 use burn::record::{FullPrecisionSettings, Recorder};
 use burn_import::pytorch::{LoadArgs, PyTorchFileRecorder};
-use burn::record::NamedMpkFileRecorder;
-use burn::module::Module;
 
 mod model;
 
 fn main() {
-    type Backend = burn::backend::NdArray<>;
+    type Backend = burn::backend::NdArray;
 
     let device = Default::default();
-    let args = LoadArgs::new("./model-2-80-dropout.pt".into()).with_debug_print();
-    let recorder  = PyTorchFileRecorder::<FullPrecisionSettings>::default();
+    let args = LoadArgs::new("./model-2-80-dropout.ptx".into())
+        .with_key_remap("net\\.1\\.weight_ih_l0.r", "gru1.reset_gate.input_transform")
+        .with_key_remap("net\\.1\\.weight_ih_l0.z", "gru1.update_gate.input_transform")
+        .with_key_remap("net\\.1\\.weight_ih_l0.n", "gru1.new_gate.input_transform")
+        .with_key_remap("net\\.1\\.weight_hh_l0.r", "gru1.reset_gate.hidden_transform")
+        .with_key_remap("net\\.1\\.weight_hh_l0.z", "gru1.update_gate.hidden_transform")
+        .with_key_remap("net\\.1\\.weight_hh_l0.n", "gru1.new_gate.hidden_transform")
+        .with_key_remap("net\\.1\\.weight_ih_l1.r", "gru2.reset_gate.input_transform")
+        .with_key_remap("net\\.1\\.weight_ih_l1.z", "gru2.update_gate.input_transform")
+        .with_key_remap("net\\.1\\.weight_ih_l1.n", "gru2.new_gate.input_transform")
+        .with_key_remap("net\\.1\\.weight_hh_l1.r", "gru2.reset_gate.hidden_transform")
+        .with_key_remap("net\\.1\\.weight_hh_l1.z", "gru2.update_gate.hidden_transform")
+        .with_key_remap("net\\.1\\.weight_hh_l1.n", "gru2.new_gate.hidden_transform")
+        .with_key_remap("net\\.3\\.(.*)", "proj.$1")
+        .with_debug_print();
+    let recorder = PyTorchFileRecorder::<FullPrecisionSettings>::default();
 
     //let recorder = NamedMpkFileRecorder::<FullPrecisionSettings>::new();
-    let record = recorder.load(args.clone(), &device).expect("Should decode state successfully");
-    let model = ModelConfig::new().init::<Backend>(&device).load_record(record);
+    let record = recorder
+        .load(args.clone(), &device)
+        .expect("Should decode state successfully");
+    let model = ModelConfig::new()
+        .init::<Backend>(&device)
+        .load_record(record);
     // model
     //     .clone()
     //     .save_file("test", &recorder)
