71cf66a0e694ea2af851b93788d8c8ac5f2520b7
Nathan Whitehead <nwhitehe@gmail.com>
2025-01-16 11:36:11 -0700

    Add embedding layer at start.

--- src/model.py
+++ src/model.py
@@ -6,16 +6,16 @@ class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, layers, num_classes):
         super(NeuralNet, self).__init__()
         layers = [
+            nn.Linear(input_size, hidden_size),
             nn.Dropout(p=0.2),
         ] + [
-            nn.GRU(input_size, hidden_size, batch_first=True),
+            nn.GRU(hidden_size, hidden_size, batch_first=True),
             SelectItem(0),
         ] + [
             nn.Dropout(p=0.2),
             nn.GRU(hidden_size, hidden_size, batch_first=True),
             SelectItem(0),
         ] * (layers - 1) + [
-            nn.Dropout(p=0.2),
             nn.Linear(hidden_size, num_classes),
         ]
         self.net = nn.Sequential(*layers)
